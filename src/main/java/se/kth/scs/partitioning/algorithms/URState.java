67817253f42155978c3978859568efe66b8b7d12
Hooman Peiro Sajjad <shps@kth.se>
2015-10-29 10:12:51 +0100

    Fixed a bug.

--- src/main/java/se/kth/scs/partitioning/algorithms/URState.java
+++ src/main/java/se/kth/scs/partitioning/algorithms/URState.java
@@ -90,6 +90,11 @@ public class URState implements PartitionState {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    @Override
+    public void releaseTaskResources() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
  
 
 }
