c9beebee2b32cb8a13650735f848e3bec7064312
Hooman <hooman.peirosajjad@data61.csiro.au>
2017-11-22 17:19:04 +1100

    Not it writes edge-partitions to file.

--- src/main/java/se/kth/scs/GraphPartitioner.java
+++ src/main/java/se/kth/scs/GraphPartitioner.java
@@ -2,26 +2,22 @@ package se.kth.scs;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.ParameterException;
-import java.io.IOException;
-import java.sql.SQLException;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
-import java.util.LinkedList;
 import se.kth.scs.partitioning.Edge;
 import se.kth.scs.partitioning.PartitionState;
-import se.kth.scs.partitioning.policy.Hdrf;
-import se.kth.scs.partitioning.policy.Greedy;
-import se.kth.scs.partitioning.policy.PartitionSelectionPolicy;
 import se.kth.scs.partitioning.hovercut.HovercutInMemoryState;
 import se.kth.scs.partitioning.hovercut.HovercutMysqlState;
 import se.kth.scs.partitioning.hovercut.HovercutPartitioner;
 import se.kth.scs.partitioning.hovercut.HovercutRemoteState;
-import se.kth.scs.utils.EdgeFileReader;
-import se.kth.scs.utils.OutputManager;
-import se.kth.scs.utils.PartitionerInputCommands;
-import se.kth.scs.utils.PartitionerSettings;
-import se.kth.scs.utils.PartitioningResult;
-import se.kth.scs.utils.PartitionsStatistics;
+import se.kth.scs.partitioning.policy.Greedy;
+import se.kth.scs.partitioning.policy.Hdrf;
+import se.kth.scs.partitioning.policy.PartitionSelectionPolicy;
+import se.kth.scs.utils.*;
+
+import java.io.*;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 
 /**
  * This class is the main class to run the graph partitioning loader.
@@ -30,176 +26,188 @@ import se.kth.scs.utils.PartitionsStatistics;
  */
 public class GraphPartitioner {
 
-  private static final OutputManager output = new OutputManager();
+    private static final OutputManager output = new OutputManager();
 
-  public static void main(String[] args) throws SQLException, IOException, Exception {
-    PartitionerInputCommands commands = new PartitionerInputCommands();
-    JCommander commander;
-    try {
-      commander = new JCommander(commands, args);
-    } catch (ParameterException ex) {
-      System.out.println(ex.getMessage());
-      System.out.println(Arrays.toString(args));
-      commander = new JCommander(commands);
-      commander.usage();
-      System.out.println(String.format("A valid command is like: %s",
-        "-f ./data/datasets/Cit-HepTh.txt -w 10 -p 4 -t 2  -s remote -db localhost:4444"));
-      System.exit(1);
-    }
-
-    runPartitioning(commands);
-    output.writeToFile(commands.output, commands.append);
-  }
-
-  private static void runPartitioning(PartitionerInputCommands commands) throws IOException, Exception {
-    PartitionerSettings settings = new PartitionerSettings();
-    settings.setSettings(commands);
-
-    System.out.println(String.format("Reading file %s", settings.file));
-    long start = System.currentTimeMillis();
-    long seed = start;
-    EdgeFileReader reader = new EdgeFileReader(settings.delimiter);
-    LinkedHashSet<Edge>[] splits = reader.readSplitFile(settings.file, 1, settings.shuffle, seed);
-    System.out.println(String.format("Finished reading in %d seconds.", (System.currentTimeMillis() - start) / 1000));
-
-    System.out.println(String.format("Re-splitting file %s", settings.file));
-    start = System.currentTimeMillis();
-    splits = EdgeFileReader.resplit(splits, settings.tasks, reader.getnEdges());
-    int nEdges = reader.getnEdges();
-    int nVertices = reader.getnVertices();
-    System.out.println(String.format("Finished resplitting in %d seconds.", (System.currentTimeMillis() - start) / 1000));
-    if (settings.window <= 0) {
-      settings.window = nEdges / settings.tasks;
-    }
-    OutputManager.printCommandSetup(settings);
-    start = System.currentTimeMillis();
-    PartitionState state = runPartitioner(settings, splits, nVertices);
-    float duration = (float) (System.currentTimeMillis() - start) / (float) 1000;
-    PartitionsStatistics ps = new PartitionsStatistics(state, nVertices);
-    String message = null;
-    if (settings.algorithm.equalsIgnoreCase(PartitionerInputCommands.HDRF)) {
-      message = String.format("HoVerCut %s, lambda=%f\tepsilon=%f", settings.algorithm, settings.lambda, settings.epsilon);
-    } else if (settings.algorithm.equalsIgnoreCase(PartitionerInputCommands.GREEDY)) {
-      message = String.format("HoVerCut %s, epsilon=%f", settings.algorithm, settings.epsilon);
-    }
-    OutputManager.printResults(settings.k, ps, message);
-    if (ps.getNVertices() != nVertices) {
-      //To check the correctness.
-      throw new Exception(String.format("Inconsistent number of vertices file=%d\tstorage=%d.", nVertices, ps.getNVertices()));
-    }
-    state.releaseResources(true);
-
-    PartitioningResult result = new PartitioningResult(
-      ps.replicationFactor(),
-      ps.maxVertexCardinality(),
-      ps.maxEdgeCardinality(),
-      ps.loadRelativeStandardDeviation(),
-      settings.window,
-      settings.tasks,
-      seed,
-      duration);
-    output.addResult(result);
-  }
-
-  private static PartitionState runPartitioner(PartitionerSettings settings, LinkedHashSet<Edge>[] splits, int nVertices) throws SQLException, IOException, Exception {
-    PartitionState state = null;
-    if (settings.exactDegree) {
-      System.out.println("Starts exact degree computation...");
-      long edStart = System.currentTimeMillis();
-      state = prepareState(settings, null, false);
-      HovercutPartitioner.computeExactDegrees(state, splits);
-      //TODO: This should be removed.
-      state.waitForAllUpdates(nVertices);
-      state.releaseTaskResources();
-      long exactDegreeTime = (int) ((System.currentTimeMillis() - edStart) / 1000);
-      System.out.println(String.format("******** Exact degree computation finished in %d seconds **********", exactDegreeTime));
-    }
-    boolean exactDegree = settings.exactDegree;
-    PartitionSelectionPolicy heuristic = buildHeuristic(settings);
-    LinkedList<Edge> partitionSortedEdges = null;
-    for (int i = 0; i <= settings.rs; i++) {
-      state = prepareState(settings, state, exactDegree);
-      if (partitionSortedEdges != null) // if it's not the first round.
-      {
-        splits = assignEdgesToSubpartitioners(partitionSortedEdges, splits.length, splits[0].size());
-      }
-
-      partitionSortedEdges = HovercutPartitioner.partitionWithWindow(
-        state,
-        splits,
-        heuristic,
-        settings.window,
-        settings.frequency,
-        exactDegree);
-      if (exactDegree == false) {
-        state.waitForAllUpdates(nVertices);
-        state.releaseTaskResources();
-      }
-    }
-    return state;
-  }
-
-  private static PartitionState prepareState(PartitionerSettings settings, PartitionState state, boolean exactDegree) throws SQLException, IOException {
-    switch (settings.storage) {
-      case PartitionerInputCommands.IN_MEMORY:
-        if (state == null || !exactDegree) {
-          state = new HovercutInMemoryState(settings.k);
-        } else {
-          state.releaseResources(false);
+    public static void main(String[] args) throws SQLException, IOException, Exception {
+        PartitionerInputCommands commands = new PartitionerInputCommands();
+        JCommander commander;
+        try {
+            commander = new JCommander(commands, args);
+        } catch (ParameterException ex) {
+            System.out.println(ex.getMessage());
+            System.out.println(Arrays.toString(args));
+            commander = new JCommander(commands);
+            commander.usage();
+            System.out.println(String.format("A valid command is like: %s",
+                    "-f ./data/datasets/Cit-HepTh.txt -w 10 -p 4 -t 2  -s remote -db localhost:4444"));
+            System.exit(1);
         }
-        break;
-      case PartitionerInputCommands.MYSQL:
-        state = new HovercutMysqlState(
-          settings.k,
-          settings.dbUrl,
-          settings.user,
-          settings.pass,
-          settings.reset);
-        break;
-      case PartitionerInputCommands.REMOTE:
-        String[] url = settings.dbUrl.split(":");
-        state = new HovercutRemoteState(settings.k, url[0], Integer.valueOf(url[1]), exactDegree);
-        break;
-      default:
-        throw new ParameterException("");
+
+        runPartitioning(commands);
+        output.writeToFile(commands.output, commands.append);
     }
 
-    return state;
-  }
+    private static void runPartitioning(PartitionerInputCommands commands) throws IOException, Exception {
+        PartitionerSettings settings = new PartitionerSettings();
+        settings.setSettings(commands);
 
-  private static PartitionSelectionPolicy buildHeuristic(PartitionerSettings settings) {
-    PartitionSelectionPolicy h = null;
-    if (settings.algorithm.equalsIgnoreCase(PartitionerInputCommands.HDRF)) {
-      h = new Hdrf(settings.lambda, settings.epsilon);
-    } else if (settings.algorithm.equalsIgnoreCase(PartitionerInputCommands.GREEDY)) {
-      h = new Greedy(settings.epsilon);
+        System.out.println(String.format("Reading file %s", settings.file));
+        long start = System.currentTimeMillis();
+        long seed = start;
+        EdgeFileReader reader = new EdgeFileReader(settings.delimiter);
+        LinkedHashSet<Edge>[] splits = reader.readSplitFile(settings.file, 1, settings.shuffle, seed);
+        System.out.println(String.format("Finished reading in %d seconds.", (System.currentTimeMillis() - start) / 1000));
+
+        System.out.println(String.format("Re-splitting file %s", settings.file));
+        start = System.currentTimeMillis();
+        splits = EdgeFileReader.resplit(splits, settings.tasks, reader.getnEdges());
+        int nEdges = reader.getnEdges();
+        int nVertices = reader.getnVertices();
+        System.out.println(String.format("Finished resplitting in %d seconds.", (System.currentTimeMillis() - start) / 1000));
+        if (settings.window <= 0) {
+            settings.window = nEdges / settings.tasks;
+        }
+        OutputManager.printCommandSetup(settings);
+        start = System.currentTimeMillis();
+        PartitionState state = runPartitioner(settings, splits, nVertices);
+        float duration = (float) (System.currentTimeMillis() - start) / (float) 1000;
+        PartitionsStatistics ps = new PartitionsStatistics(state, nVertices);
+        String message = null;
+        if (settings.algorithm.equalsIgnoreCase(PartitionerInputCommands.HDRF)) {
+            message = String.format("HoVerCut %s, lambda=%f\tepsilon=%f", settings.algorithm, settings.lambda, settings.epsilon);
+        } else if (settings.algorithm.equalsIgnoreCase(PartitionerInputCommands.GREEDY)) {
+            message = String.format("HoVerCut %s, epsilon=%f", settings.algorithm, settings.epsilon);
+        }
+        OutputManager.printResults(settings.k, ps, message);
+        if (ps.getNVertices() != nVertices) {
+            //To check the correctness.
+            throw new Exception(String.format("Inconsistent number of vertices file=%d\tstorage=%d.", nVertices, ps.getNVertices()));
+        }
+        state.releaseResources(true);
+
+        PartitioningResult result = new PartitioningResult(
+                ps.replicationFactor(),
+                ps.maxVertexCardinality(),
+                ps.maxEdgeCardinality(),
+                ps.loadRelativeStandardDeviation(),
+                settings.window,
+                settings.tasks,
+                seed,
+                duration);
+        output.addResult(result);
     }
 
-    return h;
-  }
+    private static PartitionState runPartitioner(PartitionerSettings settings, LinkedHashSet<Edge>[] splits, int nVertices) throws SQLException, IOException, Exception {
+        PartitionState state = null;
+        if (settings.exactDegree) {
+            System.out.println("Starts exact degree computation...");
+            long edStart = System.currentTimeMillis();
+            state = prepareState(settings, null, false);
+            HovercutPartitioner.computeExactDegrees(state, splits);
+            //TODO: This should be removed.
+            state.waitForAllUpdates(nVertices);
+            state.releaseTaskResources();
+            long exactDegreeTime = (int) ((System.currentTimeMillis() - edStart) / 1000);
+            System.out.println(String.format("******** Exact degree computation finished in %d seconds **********", exactDegreeTime));
+        }
+        boolean exactDegree = settings.exactDegree;
+        PartitionSelectionPolicy heuristic = buildHeuristic(settings);
+//    for (int i = 0; i <= settings.rs; i++) {
+        state = prepareState(settings, state, exactDegree);
 
-  /**
-   * Allocates edges of partitions to the subpartitioners.
-   *
-   * @param partitions
-   * @param splits
-   * @return
-   */
-  private static LinkedHashSet<Edge>[] assignEdgesToSubpartitioners(LinkedList<Edge> partitionSortedEdges, int nTasks, int size) {
-    LinkedHashSet<Edge>[] newSplits = new LinkedHashSet[nTasks];
+        LinkedList<Edge>[] partitionSortedEdges = HovercutPartitioner.partitionWithWindow(
+                state,
+                splits,
+                heuristic,
+                settings.window,
+                settings.frequency,
+                exactDegree);
+        if (exactDegree == false) {
+            state.waitForAllUpdates(nVertices);
+            state.releaseTaskResources();
+        }
+        writeToFile(settings.output, partitionSortedEdges);
+//    }
+        return state;
+    }
+
+    public static void writeToFile(
+            String output, LinkedList<Edge>[] partitions) throws FileNotFoundException {
+        String file = output + "-ep.txt";
+        File f = new File(file);
+
+        try (PrintWriter writer1 = new PrintWriter(new FileOutputStream(f, false))) {
+            for (int i = 0; i < partitions.length; i++) {
+                for (Edge e : partitions[i]) {
+                    writer1.append(String.format("%d\t%d\t%d", e.getSrc(), e.getDst(), i));
+                    writer1.append("\n");
+                }
+            }
+            writer1.flush();
+        }
+    }
+
+    private static PartitionState prepareState(PartitionerSettings settings, PartitionState state, boolean exactDegree) throws SQLException, IOException {
+        switch (settings.storage) {
+            case PartitionerInputCommands.IN_MEMORY:
+                if (state == null || !exactDegree) {
+                    state = new HovercutInMemoryState(settings.k);
+                } else {
+                    state.releaseResources(false);
+                }
+                break;
+            case PartitionerInputCommands.MYSQL:
+                state = new HovercutMysqlState(
+                        settings.k,
+                        settings.dbUrl,
+                        settings.user,
+                        settings.pass,
+                        settings.reset);
+                break;
+            case PartitionerInputCommands.REMOTE:
+                String[] url = settings.dbUrl.split(":");
+                state = new HovercutRemoteState(settings.k, url[0], Integer.valueOf(url[1]), exactDegree);
+                break;
+            default:
+                throw new ParameterException("");
+        }
+
+        return state;
+    }
+
+    private static PartitionSelectionPolicy buildHeuristic(PartitionerSettings settings) {
+        PartitionSelectionPolicy h = null;
+        if (settings.algorithm.equalsIgnoreCase(PartitionerInputCommands.HDRF)) {
+            h = new Hdrf(settings.lambda, settings.epsilon);
+        } else if (settings.algorithm.equalsIgnoreCase(PartitionerInputCommands.GREEDY)) {
+            h = new Greedy(settings.epsilon);
+        }
+
+        return h;
+    }
+
+    /**
+     * Allocates edges of partitions to the subpartitioners.
+     *
+     * @param partitions
+     * @param splits
+     * @return
+     */
+    private static LinkedHashSet<Edge>[] assignEdgesToSubpartitioners(LinkedList<Edge> partitionSortedEdges, int nTasks, int size) {
+        LinkedHashSet<Edge>[] newSplits = new LinkedHashSet[nTasks];
 
 //    int counter = 0;
-    for (int i = 0; i < nTasks; i++) {
-      newSplits[i] = new LinkedHashSet<>();
-      for (int j = 0; j < size; j++) {
-        newSplits[i].add(partitionSortedEdges.poll());
+        for (int i = 0; i < nTasks; i++) {
+            newSplits[i] = new LinkedHashSet<>();
+            for (int j = 0; j < size; j++) {
+                newSplits[i].add(partitionSortedEdges.poll());
 //        counter++;
-        if (partitionSortedEdges.isEmpty()) {
-          break;
+                if (partitionSortedEdges.isEmpty()) {
+                    break;
+                }
+            }
         }
-      }
-    }
 
-    return newSplits;
-  }
+        return newSplits;
+    }
 }
--- src/main/java/se/kth/scs/partitioning/hovercut/HovercutPartitioner.java
+++ src/main/java/se/kth/scs/partitioning/hovercut/HovercutPartitioner.java
@@ -2,6 +2,7 @@ package se.kth.scs.partitioning.hovercut;
 
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
+
 import se.kth.scs.partitioning.Edge;
 import se.kth.scs.partitioning.PartitionState;
 import se.kth.scs.partitioning.policy.PartitionSelectionPolicy;
@@ -15,82 +16,81 @@ import se.kth.scs.partitioning.policy.PartitionSelectionPolicy;
  */
 public class HovercutPartitioner {
 
-  public static final double DEFAULT_LAMBDA = 1;
-  public static final double DEFAULT_EPSILON = 1;
+    public static final double DEFAULT_LAMBDA = 1;
+    public static final double DEFAULT_EPSILON = 1;
 
-  /**
-   *
-   * @param hState
-   * @param edges
-   * @param heuristic
-   * @param windowSize
-   * @param pUpdateFrequency
-   * @param exactDegree
-   * @return
-   */
-  public static LinkedList<Edge> partitionWithWindow(
-    PartitionState hState, LinkedHashSet<Edge> edges[],
-    PartitionSelectionPolicy heuristic,
-    int windowSize,
-    int pUpdateFrequency,
-    boolean exactDegree) {
-    System.out.println("Starts partitioning...");
-    int nTasks = edges.length;
-    Subpartitioner[] tasks = new Subpartitioner[nTasks];
-    Thread[] threads = new Thread[nTasks];
-    for (int i = 0; i < nTasks; i++) {
-      tasks[i] = new Subpartitioner(
-        hState,
-        edges[i],
-        heuristic,
-        windowSize,
-        pUpdateFrequency,
-        exactDegree);
-      threads[i] = new Thread(tasks[i]);
-    }
+    /**
+     * @param hState
+     * @param edges
+     * @param heuristic
+     * @param windowSize
+     * @param pUpdateFrequency
+     * @param exactDegree
+     * @return
+     */
+    public static LinkedList<Edge>[] partitionWithWindow(
+            PartitionState hState, LinkedHashSet<Edge> edges[],
+            PartitionSelectionPolicy heuristic,
+            int windowSize,
+            int pUpdateFrequency,
+            boolean exactDegree) {
+        System.out.println("Starts partitioning...");
+        int nTasks = edges.length;
+        Subpartitioner[] tasks = new Subpartitioner[nTasks];
+        Thread[] threads = new Thread[nTasks];
+        for (int i = 0; i < nTasks; i++) {
+            tasks[i] = new Subpartitioner(
+                    hState,
+                    edges[i],
+                    heuristic,
+                    windowSize,
+                    pUpdateFrequency,
+                    exactDegree);
+            threads[i] = new Thread(tasks[i]);
+        }
 
-    long start = System.currentTimeMillis();
-    executeThreads(threads);
-    System.out.println(String.format("******** Partitioning finished in %d seconds **********", (System.currentTimeMillis() - start) / 1000));
+        long start = System.currentTimeMillis();
+        executeThreads(threads);
+        System.out.println(String.format("******** Partitioning finished in %d seconds **********", (System.currentTimeMillis() - start) / 1000));
 
 //    LinkedList<Edge>[][] outputAssignments = new LinkedList[nTasks][hState.getNumberOfPartitions()];
 //    for (int i = 0; i < nTasks; i++) {
 //      outputAssignments[i] = tasks[i].getAssignments();
 //    }
-    LinkedList<Edge> outputAssignments = new LinkedList();
-    for (int i = 0; i < hState.getNumberOfPartitions(); i++) {
-      for (Subpartitioner task : tasks) {
-        outputAssignments.addAll(task.getAssignments()[i]);
-
-      }
-    }
+        LinkedList<Edge>[] outputAssignments = new LinkedList[hState.getNumberOfPartitions()];
+        for (int i = 0; i < hState.getNumberOfPartitions(); i++) {
+            outputAssignments[i] = new LinkedList();
+            for (Subpartitioner task : tasks) {
+                outputAssignments[i].addAll(task.getAssignments()[i]);
+            }
+        }
 //    System.out.println(String.format("******** edges after resplitting %d **********", outputAssignments.size()));
-    return outputAssignments;
-  }
-
-  public static void computeExactDegrees(PartitionState hState, LinkedHashSet<Edge> edges[]) {
-    int nTasks = edges.length;
-    ExactDegreeTask[] tasks = new ExactDegreeTask[nTasks];
-    Thread[] threads = new Thread[nTasks];
-    for (int i = 0; i < nTasks; i++) {
-      tasks[i] = new ExactDegreeTask(hState, edges[i]);
-      threads[i] = new Thread(tasks[i]);
-    }
-    executeThreads(threads);
-  }
-
-  private static void executeThreads(Thread[] threads) {
-    System.out.println("Start Running Tasks!");
-    for (Thread t : threads) {
-      t.start();
+        return outputAssignments;
     }
 
-    for (Thread t : threads) {
-      try {
-        t.join();
-      } catch (InterruptedException ex) {
-        System.out.println(ex.getMessage());
-      }
+    public static void computeExactDegrees(PartitionState hState, LinkedHashSet<Edge> edges[]) {
+        int nTasks = edges.length;
+        ExactDegreeTask[] tasks = new ExactDegreeTask[nTasks];
+        Thread[] threads = new Thread[nTasks];
+        for (int i = 0; i < nTasks; i++) {
+            tasks[i] = new ExactDegreeTask(hState, edges[i]);
+            threads[i] = new Thread(tasks[i]);
+        }
+        executeThreads(threads);
     }
-  }
+
+    private static void executeThreads(Thread[] threads) {
+        System.out.println("Start Running Tasks!");
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        for (Thread t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
 }
